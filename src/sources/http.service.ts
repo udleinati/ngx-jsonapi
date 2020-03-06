e1e24360255398ad88e9e730613e68a487360325
Udlei Nati <udlei.nati@free2move.me>
2020-03-06 16:38:14 +0000

    fix url when using ssr

--- src/sources/http.service.ts
+++ src/sources/http.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID, Optional } from '@angular/core';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
 import { IDocumentResource } from '../interfaces/data-object';
-import { HttpClient, HttpHeaders, HttpEvent } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { isPlatformServer } from '@angular/common';
 import { JsonapiConfig } from '../jsonapi-config';
 import { share, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -11,9 +13,10 @@ export class Http {
     // NOTE: GET requests are stored in a this object to prevent duplicate requests
     public get_requests: { [key: string]: Observable<IDocumentData> } = {};
 
-    public constructor(private http: HttpClient, private rsJsonapiConfig: JsonapiConfig) {}
+    public constructor(@Optional() @Inject(REQUEST) private request: any, @Inject(PLATFORM_ID) private platformId: any, private http: HttpClient, private rsJsonapiConfig: JsonapiConfig) {}
 
     public exec(path: string, method: string, data?: IDocumentResource): Observable<IDocumentData> {
+        let url = this.rsJsonapiConfig.url
         let req = {
             body: data || null,
             headers: new HttpHeaders({
@@ -22,10 +25,17 @@ export class Http {
             })
         };
 
+        if (isPlatformServer(this.platformId) && !url.match(/^http\:|^https\:|^\/\//)) {
+            const headers = this.request.headers
+            const proto = (headers['x-forwarded-proto']) ? headers['x-forwarded-proto'].split(',')[0] : (headers['proto']) ? headers['proto'] : 'http'
+            const host = (headers['x-forwarded-host']) ? headers['x-forwarded-host'].split(',')[0] : headers['host']
+            url = `${proto}://${host}${url}`
+        }
+
         // NOTE: prevent duplicate GET requests
         if (method === 'get') {
             if (!this.get_requests[path]) {
-                let obs = this.http.request<IDocumentData>(method, this.rsJsonapiConfig.url + path, req).pipe(
+                let obs = this.http.request<IDocumentData>(method, url + path, req).pipe(
                     tap(() => {
                         delete this.get_requests[path];
                     }),
@@ -39,7 +49,7 @@ export class Http {
             return this.get_requests[path];
         }
 
-        return this.http.request<IDocumentData>(method, this.rsJsonapiConfig.url + path, req).pipe(
+        return this.http.request<IDocumentData>(method, url + path, req).pipe(
             tap(() => {
                 delete this.get_requests[path];
             }),
