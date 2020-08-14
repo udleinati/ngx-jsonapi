84514f740784413bfac0375801cd2c3483f362f4
Udlei Nati <udlei.nati@free2move.me>
2020-08-14 23:37:20 +0100

    improvements

--- src/core.ts
+++ src/core.ts
@@ -9,8 +9,7 @@ import { JsonapiConfig } from './jsonapi-config';
 import { Http as JsonapiHttpImported } from './sources/http.service';
 import { StoreService as JsonapiStore } from './sources/store.service';
 import { IDocumentResource } from './interfaces/data-object';
-import { noop } from 'rxjs/internal/util/noop';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, noop } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { IDocumentData } from './interfaces/document';
 
